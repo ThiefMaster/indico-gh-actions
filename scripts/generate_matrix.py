22807d3bc248e8592ca8793a6baa67f7f781d9b8
Adrian Moennich <adrian.moennich@cern.ch>
2024-08-12 17:37:48 +0200

    Use pyproject.toml instead of setup.cfg

--- scripts/generate_matrix.py
+++ scripts/generate_matrix.py
@@ -3,10 +3,9 @@ import os
 import re
 import subprocess
 import sys
+import tomllib
 from operator import itemgetter
 from pathlib import Path
-
-from setuptools.config.setupcfg import read_configuration
 
 
 def _get_pkg_dir(plugin_dir: Path):
@@ -33,35 +32,8 @@ def _plugin_has_i18n(plugin_dir: Path):
     return (pkg_dir / 'translations').exists()
 
 
-def _plugin_has_invalid_manifest(plugin_dir: Path):
-    pkg_dir = _get_pkg_dir(plugin_dir)
-    if not pkg_dir:
-        return
-    data_dirs = [
-        sub.name
-        for sub in pkg_dir.iterdir()
-        if sub.name not in {'__pycache__', 'client'} and sub.is_dir() and not any(sub.glob('*.py'))
-    ]
-    if not data_dirs:
-        return False
-    expected_manifest = {f'graft {pkg_dir.name}/{plugin_dir}' for plugin_dir in data_dirs}
-    manifest_file = plugin_dir / 'MANIFEST.in'
-    if not manifest_file.exists():
-        print(f'::error::{plugin_dir.name} has no manifest')
-        for line in expected_manifest:
-            print(f'::error::manifest entry missing: {line}')
-        return True
-    manifest_lines = set(manifest_file.read_text().splitlines())
-    if missing := (expected_manifest - manifest_lines):
-        print(f'::error::{plugin_dir.name} has incomplete manifest')
-        for line in missing:
-            print(f'::error::manifest entry missing: {line}')
-        return True
-    return False
-
-
 def _get_plugin_deps(plugin_dir: Path):
-    reqs = read_configuration(plugin_dir / 'setup.cfg')['options']['install_requires']
+    reqs = tomllib.loads((plugin_dir / 'pyproject.toml').read_text())['project']['dependencies']
     return [
         re.match(r'indico-plugin-([^>=<]+)', x).group(1).replace('-', '_')
         for x in reqs
@@ -83,7 +55,6 @@ def _get_plugin_data(plugin_dir: Path, *, single=False):
         'assets': _plugin_has_assets(plugin_dir) if not meta else False,
         'i18n': _plugin_has_i18n(plugin_dir) if not meta else False,
         'deps': _get_plugin_deps(plugin_dir) if not meta else [],
-        'invalid_manifest': _plugin_has_invalid_manifest(plugin_dir) if not meta else False,
         'single': single,
     }
 
@@ -101,16 +72,16 @@ def _get_changed_dirs():
 
 
 def main():
-    if Path('setup.cfg').exists():
+    if Path('pyproject.toml').exists():
         # single-plugin repo
         plugin_data = [_get_plugin_data('..' / Path(Path().absolute().name), single=True)]
     else:
         # multi-plugin repo
         plugin_data = sorted(
-            (_get_plugin_data(x) for x in Path().iterdir() if x.is_dir() and (x / 'setup.cfg').exists()),
+            (_get_plugin_data(x) for x in Path().iterdir() if x.is_dir() and (x / 'pyproject.toml').exists()),
             key=itemgetter('plugin'),
         )
-        # Filter out untouched plugin if we're running for a PR
+        # Filter out untouched plugins if we're running for a PR
         if os.environ['GITHUB_EVENT_NAME'] == 'pull_request':
             print('::notice title=PR mode::Adding plugins touched in this PR to matrix')
             changed_dirs = _get_changed_dirs()
